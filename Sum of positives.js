2ab0592e69ebbcbd143267d71810ceff258a2cee
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-02-08 10:42:42 +0100

    Update Sum of positives.js

--- Sum of positives.js
+++ Sum of positives.js
@@ -22,3 +22,9 @@ function positiveSum(arr) {
 function positiveSum(arr) {
    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
 }
+
+3.
+function positiveSum (arr) {
+  return arr.filter(x => x>=0).reduce((a, c) => a + c, 0);
+}
+
