9f14dc3ef3a7f7b47543b4a72b9195d890e4eb85
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-02-12 07:42:56 +0100

    Update Count of positives:sum of negatives.js

--- Count of positives:sum of negatives.js
+++ Count of positives:sum of negatives.js
@@ -34,6 +34,15 @@ function countPositivesSumNegatives(input) {
     
     return [positive, negative];
 }
+3.
+function countPositivesSumNegatives(input) {
+  if (!Array.isArray(input) || !input.length) return [];
+  return input.reduce((arr, n) => {
+    if (n > 0) arr[0]++;
+    if (n < 0) arr[1] += n;
+    return arr;
+  }, [0, 0]);
+}
 
 
 
