140a682a372b61e6df7e22697396e8d9e7cdf266
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-06-04 11:19:39 +0200

    Update Get the mean in an array.js

--- Get the mean in an array.js
+++ Get the mean in an array.js
@@ -1,6 +1,13 @@
+//1.
 function getAverage(marks){
     let acc=0;
     marks.forEach(el=>acc+=el);
     return Math.floor(acc/marks.length);
+}
+//2.
+
+function getAverage(marks){
+
+    return Math.floor(marks.reduce((a,b)=>a+b,0)/marks.length);
 }
 
