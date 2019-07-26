fba21b09d985fb8159b48d32f850c13295c159bd
Natalya <natalyamyunster@yahoo.com>
2019-07-26 19:01:25 +0200

    add solution/SwitchItUp

---
+++ SwitchItUp.js
@@ -0,0 +1,17 @@
+/*When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+Try using "Switch" statements.
+
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+
+ */
+
+
+//My solution:
+
+
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n];
